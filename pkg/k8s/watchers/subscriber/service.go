8f7d5e87b5a87612697f27d4075bf8fc00d5a36e
Chris Tarazi <chris@isovalent.com>
2021-04-04 17:29:49 -0700

    k8s/watchers/subscriber: Disambiguate API by object type
    The initial implementation of this package assumed that Golang allowed
    methods of the same name but differing signatures to be defined. That is
    not the case. For example, the following code doesn't compile:
    
    func (o *object) OnUpdate(t T) error {}
    func (o *object) OnUpdate(u U) error {}
    
    To ease the users of the subscriber package, this commit disambiguates
    the various *Handler interfaces. Now, *Handler interface implementations
    must have methods with the object name specified, such as
    OnUpdateService(), for the ServiceHandler. The reason is to avoid a
    situation where a single object that implements multiple `*Handler`s,
    for example to react to K8s service and node events, can collide.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>

--- pkg/k8s/watchers/subscriber/service.go
+++ pkg/k8s/watchers/subscriber/service.go
@@ -10,9 +10,9 @@ import (
 // ServiceHandler is implemented by event handlers responding to K8s Service
 // events.
 type ServiceHandler interface {
-	OnAdd(*slim_corev1.Service) error
-	OnUpdate(oldObj, newObj *slim_corev1.Service) error
-	OnDelete(*slim_corev1.Service) error
+	OnAddService(*slim_corev1.Service) error
+	OnUpdateService(oldObj, newObj *slim_corev1.Service) error
+	OnDeleteService(*slim_corev1.Service) error
 }
 
 // NewService creates a new subscriber list for ServiceHandlers.
@@ -34,7 +34,7 @@ func (l *ServiceList) NotifyAdd(svc *slim_corev1.Service) []error {
 	defer l.RUnlock()
 	errs := make([]error, 0, len(l.subs))
 	for _, s := range l.subs {
-		if err := s.OnAdd(svc); err != nil {
+		if err := s.OnAddService(svc); err != nil {
 			errs = append(errs, err)
 		}
 	}
@@ -47,7 +47,7 @@ func (l *ServiceList) NotifyUpdate(oldSvc, newSvc *slim_corev1.Service) []error 
 	defer l.RUnlock()
 	errs := make([]error, 0, len(l.subs))
 	for _, s := range l.subs {
-		if err := s.OnUpdate(oldSvc, newSvc); err != nil {
+		if err := s.OnUpdateService(oldSvc, newSvc); err != nil {
 			errs = append(errs, err)
 		}
 	}
@@ -60,7 +60,7 @@ func (l *ServiceList) NotifyDelete(svc *slim_corev1.Service) []error {
 	defer l.RUnlock()
 	errs := make([]error, 0, len(l.subs))
 	for _, s := range l.subs {
-		if err := s.OnDelete(svc); err != nil {
+		if err := s.OnDeleteService(svc); err != nil {
 			errs = append(errs, err)
 		}
 	}
